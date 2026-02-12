5bf1e6f9ad0356ed95130760e3933c6b621f4bc1
sila_senapong <sila_sanapong@cmu.ac.th>
2026-02-12 23:33:27 +0700

    add file

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -16,15 +16,30 @@ double overlap(Rect R1, Rect R2)
 	double EdgeLeft_2 = R2.x, EdgeRight_2 = R2.x + R2.w,
 		   EdgeTop_2 = R2.y, EdgeBottom_2 = R2.y + R2.h;
 
-	double Left_inter, Right_inter, Top_inter, Bottom_inter;
+	double Left_inter, Right_inter, Top_inter, Bottom_inter, width, height;
 
-	Left_inter = max(EdgeLeft_1, EdgeLeft_2),
-	Right_inter = min(EdgeRight_1, EdgeRight_2),
-	Top_inter = max(EdgeTop_1, EdgeTop_2),
+	Left_inter = max(EdgeLeft_1, EdgeLeft_2);
+	Right_inter = min(EdgeRight_1, EdgeRight_2);
+	Top_inter = max(EdgeTop_1, EdgeTop_2);
 	Bottom_inter = min(EdgeBottom_1, EdgeBottom_2);
 
-	double width = Right_inter - Left_inter,
-		   height = Bottom_inter - Top_inter;
+	if (EdgeLeft_1 < EdgeLeft_2 && EdgeRight_1 > EdgeRight_2 &&
+		EdgeTop_1 > EdgeTop_2 && EdgeBottom_1 > EdgeBottom_2)
+	{
+		width = R2.w;
+		height = R2.h;
+	}
+	else if (EdgeLeft_1 > EdgeLeft_2 && EdgeRight_1 < EdgeRight_2 &&
+			 EdgeTop_1 < EdgeTop_2 && EdgeBottom_1 < EdgeBottom_2)
+	{
+		width = R1.w;
+		height = R1.h;
+	}
+	else
+	{
+		width = Right_inter - Left_inter;
+		height = Bottom_inter - Top_inter;
+	}
 
 	if (width <= 0 || height <= 0)
 	{
@@ -50,6 +65,10 @@ int main()
 	Rect R5 = {-1, 2, 6.9, 9.6};
 	Rect R6 = {0, 0, 1.2, 2.5};
 	cout << overlap(R5, R6) << endl;
+
+	Rect R7 = {0, 0, 1.2, 2.5};
+	Rect R8 = {-1, 2, 6.9, 9.6};
+	cout << overlap(R7, R8) << endl;
 
 	return 0;
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -1,123 +1,151 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<vector>
-#include<cstdlib>
-#include<iomanip>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
-struct student{
-
-    //[Missing Code 1] Define struct student with four members (name ,id , gender, gpa);
-    
+struct student
+{
+	string name;
+	int id;
+	char gender;
+	double gpa;
+	//[Missing Code 1] Define struct student with four members (name ,id , gender, gpa);
 };
 
-struct course{
+struct course
+{
 	string name;
 	int id;
 	vector<string> lecture_list;
 	vector<student *> student_list;
 };
 
+student text2student(string text)
+{
+	student s;
+	string name = "", id = "", gen = "", gpa = "";
+	int count = 0;
+	for (unsigned int i = 0; i < text.length(); i++)
+	{
+		if (text[i] == ',')
+			count++;
+		else if (count == 0)
+			name += text[i];
+		else if (count == 1)
+			id += text[i];
+		else if (count == 2)
+			gen += text[i];
+		else if (count == 3)
+			gpa += text[i];
+	}
 
-student text2student(string text){
-    student s;
-    string name = "", id = "", gen = "", gpa = "";
-    int count = 0;
-    for(unsigned int i = 0; i < text.length(); i++){
-        if(text[i] == ',') count++; 
-        else if(count == 0) name += text[i];
-        else if(count == 1) id += text[i];
-        else if(count == 2) gen += text[i];
-        else if(count == 3) gpa += text[i];
-    }
-    
-    //[Missing Code 2] Fill in the blank with the correct code.;
-    s.name = name;
-    s.id = _____________;
-    s.gender = _____________;
-    s.gpa = _____________;
-    
-    _____________;
+	//[Missing Code 2] Fill in the blank with the correct code.;
+	s.name = name;
+	s.id = _____________;
+	s.gender = _____________;
+	s.gpa = _____________;
+
+	_____________;
 }
 
-
-student * findstudent(vector<student> allstudents,int key){ //[Missing Code 4] There is something wrong in this line.
-	for(unsigned int i = 0; i < allstudents.size(); i++){
-		if(allstudents[i].id  == key) return &allstudents[i];
+student *findstudent(vector<student> allstudents, int key)
+{ //[Missing Code 4] There is something wrong in this line.
+	for (unsigned int i = 0; i < allstudents.size(); i++)
+	{
+		if (allstudents[i].id == key)
+			return &allstudents[i];
 	}
 	return 0;
 }
 
-void printreport(vector<course> allcourses){
-	for(unsigned int i = 0;i < allcourses.size(); i++){
+void printreport(vector<course> allcourses)
+{
+	for (unsigned int i = 0; i < allcourses.size(); i++)
+	{
 		cout << "-----------------------------------------------------------------------------\n";
 		cout << left << setw(20);
-		cout << "Course:" << allcourses[i].name << " ("<< allcourses[i].id << ")\n\n";
+		cout << "Course:" << allcourses[i].name << " (" << allcourses[i].id << ")\n\n";
 		cout << left << setw(20);
 		cout << "Lecturers:";
-		for(unsigned int j = 0; j < allcourses[i].lecture_list.size();j++){
-			if(j != 0) cout << ", ";
+		for (unsigned int j = 0; j < allcourses[i].lecture_list.size(); j++)
+		{
+			if (j != 0)
+				cout << ", ";
 			cout << allcourses[i].lecture_list[j];
 		}
 		cout << "\n\n";
 		cout << left << setw(20);
 		cout << "Students:";
-		for(unsigned int j = 0; j < allcourses[i].student_list.size();j++){
-			if(j != 0) cout << left << setw(20) << " ";
+		for (unsigned int j = 0; j < allcourses[i].student_list.size(); j++)
+		{
+			if (j != 0)
+				cout << left << setw(20) << " ";
 			cout << left << setw(20) << allcourses[i].student_list[j]->name;
 			cout << left << setw(10) << allcourses[i].student_list[j]->id;
 			cout << left << setw(3) << allcourses[i].student_list[j]->gender;
 			cout << left << setw(5) << setprecision(2) << fixed << allcourses[i].student_list[j]->gpa << "\n";
-		} 
-		
+		}
 	}
 	cout << "-----------------------------------------------------------------------------\n";
 }
 
-int main(){
+int main()
+{
 	ifstream student_file("students.txt");
 	ifstream course_file("courses.txt");
 	vector<student> allstudents;
 	vector<course> allcourses;
-	
+
 	string textline;
-	
-	while(getline(student_file,textline)){
-        student s =  text2student(textline); 
-		allstudents.push_back(s); 		
+
+	while (getline(student_file, textline))
+	{
+		student s = text2student(textline);
+		allstudents.push_back(s);
 	}
-	
+
 	int state = 1;
-	while(getline(course_file,textline)){
-		if(state == 1){
+	while (getline(course_file, textline))
+	{
+		if (state == 1)
+		{
 			course c;
 			int loc = textline.find_first_of('(');
-			c.name = textline.substr(0,loc-1);
-			c.id = atof(textline.substr(loc+1,5).c_str());
-			getline(course_file,textline);
+			c.name = textline.substr(0, loc - 1);
+			c.id = atof(textline.substr(loc + 1, 5).c_str());
+			getline(course_file, textline);
 			allcourses.push_back(c);
-			state = 2;			
-		}else if(state == 2){
-			if(textline == "> Students"){
+			state = 2;
+		}
+		else if (state == 2)
+		{
+			if (textline == "> Students")
+			{
 				state = 3;
-			}else{
-			
-			    //[Missing Code 3] Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
-			}			
-		}else{
-			if(textline == "---------------------------------------"){
+			}
+			else
+			{
+
+				//[Missing Code 3] Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
+			}
+		}
+		else
+		{
+			if (textline == "---------------------------------------")
+			{
 				state = 1;
-			}else{
-				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+			}
+			else
+			{
+				student *p = findstudent(allstudents, atof(textline.c_str()));
+
 				//[Missing Code 5] Append (push_back) p to student_list of the recently added course in allcourses[];
-				
 			}
 		}
 	}
 	printreport(allcourses);
-	
 }
